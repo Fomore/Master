b9c813f9b5c8659c4e90b23f84e7018069c58211
Fomore <falko.benezan@student.uni-tuebingen.de>
2017-06-01 13:31:16 +0200

    Auswerten aller Augen und speichern

--- SynthEyesInterpreter.py
+++ SynthEyesInterpreter.py
@@ -1,34 +1,37 @@
 import cPickle
 import cv2
 import numpy as np
+import fnmatch
+import os
 
-f = open('/home/falko/Uni/Master/Bilder/SynthEyes_data/f01/f01_42_-0.0000_-0.1963.pkl', 'rb')
-db = cPickle.load(f)
-f.close()
-
-#print(db['ldmks']['ldmks_iris_2d'])
-
-image = cv2.imread('/home/falko/Uni/Master/Bilder/SynthEyes_data/f01/f01_42_-0.0000_-0.1963.png')
+out = open('workfile.txt', 'w+')
 #(x_centre,y_centre),(minor_axis,major_axis),angle
+for root, dirnames, filenames in os.walk('/home/falko/Uni/Master/Bilder/SynthEyes_data/'):
+    for filename in fnmatch.filter(filenames, '*.png'):
+        path = os.path.join(root, filename);
+        path2 = path[:-2]+'kl'
+        f = open(path2, 'rb')
+        db = cPickle.load(f)
+        f.close()
+        iris = np.array(db['ldmks']['ldmks_iris_2d']).astype(float32)
+        pupil = np.array(db['ldmks']['ldmks_pupil_2d']).astype(float32)
+        lids = np.array(db['ldmks']['ldmks_lids_2d']).astype(float32)
+        ellipse_i = cv2.fitEllipse(iris)
+        ellipse_p = cv2.fitEllipse(pupil)
+        box = cv2.boundingRect(lids)
 
-mImagePaths = cv2.glob('/home/falko/Uni/Master/Bilder/SynthEyes_data/*.png')
-print(mImagePaths)
-
-iris = np.array(db['ldmks']['ldmks_iris_2d']).astype(float32)
-pupil = np.array(db['ldmks']['ldmks_pupil_2d']).astype(float32)
-print(iris)
-ellipse_i = cv2.fitEllipse(iris)
-ellipse_p = cv2.fitEllipse(pupil)
-cv2.ellipse(image,ellipse_i,(0,255,0))
-cv2.ellipse(image,ellipse_p,(0,0,255))
-print(ellipse_i)
-print(ellipse_p)
-for i in range(len(iris)):
-    image[int(round(iris[i,1])),int(round(iris[i,0]))] = (255,255,0)
-for i in range(len(pupil)):
-    image[int(round(pupil[i,1])),int(round(pupil[i,0]))] = (255,0,255)
-
-cv2.imwrite('TestAuge.png',image)
-cv2.imshow("Image", image)
-cv2.waitKey(300)
+#        cv_img = cv2.imread(path)
+#        cv2.ellipse(cv_img,ellipse_i,(0,255,0))
+#        cv2.ellipse(cv_img,ellipse_p,(0,0,255))
+#        cv2.rectangle(cv_img,(x,y),(x+w,y+h),(0,255,0),2)
+#        cv2.imshow("Image", cv_img)
+#        cv2.waitKey(30)
+        out.write(path2+' ')
+        out.write(str(ellipse_i))
+        out.write(' ')
+        out.write(str(ellipse_p))
+        out.write(' ')
+        out.write(str(box))
+        out.write('\n')
 cv2.destroyAllWindows()
+print("Ende")
